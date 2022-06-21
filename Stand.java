b28ddfaae7774a01b90d423b000bec5260ea64bb
Padnure27 <shwetaxworkz@gmail.com>
2022-06-21 12:04:21 +0530

    commit message

---
+++ Stand.java
@@ -0,0 +1,11 @@
+class Stand{
+  public Stand(){
+    System.out.prinln("Stand object created");
+  }
+  public Stand(){
+ //Constructor name &class both are same
+// constructor no return type   
+//javap=decompile 
+//we didnot write static i.e instance method
+// javac=compile 
+System.out.println("   
